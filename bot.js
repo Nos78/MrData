df6cf2f7a7b95dbb63dc51095dc889f6b2f07029
Nos78 <44951782+Nos78@users.noreply.github.com>
2019-03-10 00:18:37 +0000

    Update to use Postgres

--- bot.js
+++ bot.js
@@ -1,13 +1,23 @@
+// ============================
+// MrData bot.js
+// ============================
+// Created by BanderDragon#5699
+// (c) 2019
+// noscere1978@gmail.com
+// ============================
+
+// Configure the Discord bot client
 const Discord = require('discord.js');
 const config = require('./config.json');
 
-const SQLite = require("better-sqlite3");
-const sql = new SQLite('./scores.sqlite');
 
-var logger = require('winston');
-var fs  = require('fs');
+//const SQLite = require("better-sqlite3");
+//const sql = new SQLite('./scores.sqlite');
+const pgp = require('pg-promise')(/*options*/)
+const sql = pgp('postgres://username:password@host:port/database')
 
-// Configure logger settings
+// Set up the logger for debug/info
+const logger = require('winston');
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
   colorize: true
@@ -15,34 +25,40 @@ logger.add(new logger.transports.Console, {
 
 logger.level = 'debug';
 
+
 // Initialize Discord Bot
+// token needs to be added to config.json
 const bot = new Discord.Client({
  token: config.token,
  autorun: true
 });
 
-// Configure score-keeping (currently FS, consider using SQL)
-const pd = require("./scores/pd.json");
-const rr = require("./scores/rr.json");
-typeof score; // object
-
-bot.on("ready", () => {
+//
+// Set up the callback functions
+//
+//
+// bot.on ready - used when the bot comes online
+//
+$bot.on("ready", () => {
   logger.info('Connected');
   logger.info('Logged in as: ');
   logger.info(bot.user.username + ' - (' + bot.user.id + ')');
+  // Update the bot activity text to reflect the connections status
   bot.user.setActivity(`${bot.guilds.size} guilds | ${config.prefix}datahelp`, { type: 'WATCHING'});
-  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
+  logger.info(`MrData Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
 
-  // Some database gubbins
-  // Check if the table "points" exists.
+  // We want to ensure our table is created when the bot comes online
+  // and configure them if they don't exist
   const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
   if (!table['count(*)']) {
+    logger.debug(`No database score table found!  Creating...`);
     // If the table isn't there, create it and setup the database correctly.
     sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, power_destroyed INTEGER, resources_raided INTEGER);").run();
     // Ensure that the "id" row is always unique and indexed.
     sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
     sql.pragma("synchronous = 1");
     sql.pragma("journal_mode = wal");
+    logger.debug(`Database configured.`)
   }
 
   // And then we have two prepared statements to get and set the score data.
@@ -50,15 +66,22 @@ bot.on("ready", () => {
   bot.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, power_destroyed, resources_raided) VALUES (@id, @user, @guild, @power_destroyed, @resources_raided);");
 });
 
+//
+// guildCreate & guildDelete
+// These two events are triggered when the bot joins and leaves
+// a guild server...
+//
 bot.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  logger.info(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  // Update the bot activity text to reflect the new stat
   bot.user.setActivity(`${bot.guilds.size} guilds | ${config.prefix}datahelp`, { type: 'WATCHING'});
 });
 
 bot.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  logger.info(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  // Update the bot activity text to reflect the new stat
   bot.user.setActivity(`${bot.guilds.size} guilds | ${config.prefix}datahelp`, { type: 'WATCHING'});
 });
 
@@ -77,7 +100,8 @@ bot.on('message', async message => {
   const sender = message.author;
   const senderID = message.author.id;
 
-  logger.info(`${sender}`);
+  logger.debug(`${sender} ${cmd}`);
+
   switch(cmd) {
     // !ping
     case 'ping':
@@ -92,14 +116,14 @@ bot.on('message', async message => {
         to: message.channel,
         message: '`!ping           : see if I am awake\n' +
                   '!datahelp         : self explanatory!\n' +
+                  '\n' +
                   '!powerdestroyed : see the powerdestroyed league table (alias = !pd).\n' +
                   '!powerdestroyed <number> : set the level of your power destroyed to the <number> you specify.\n' +
-                  //'!powerdestroyed <name> : see the power destroyed of <name>.\n' +
-                  //'!powerdestroyed <name> <number> : set the power destroyed of <name> to <number> - bot admins only.\n' +
+                  '!powerdestroyed <name> : see the power destroyed of <name>.\n' +
+                  '\n' +
                   '!resourcesraided : see the resourcesraided league table (alias = !rr).\n' +
                   '!resourcesraided <number> : set the level of your resourcesraided to the <number> you specify.\n' +
-                  //'!resourcesraided <name> : see the resourcesraided of <name>.\n' +
-                  //'!resourcesraided <name> <number> : set the resourcesraided of <name> to <number> - bot admins only.`\n' +
+                  '!resourcesraided <name> : see the resourcesraided of <name>.\n' +
                   ''
       });
     break;
@@ -129,10 +153,19 @@ bot.on('message', async message => {
               description: `Thank you, ${sender}, your power destroyed is set to ${args[0]}`
           }});
         } else {
-          message.channel.send({embed: {
+          let member = message.guild.members.get(args[0]);
+          if(member) {
+            let score = client.getScore.get(member.id, message.guild.id);
+            message.channel.send({embed: {
               color: 3447003,
-              description: `${sender}, please use \`!pd number\`, where number is an actual number!}`
-          }});
+              description: `${args[0]} power destroyed is ${score.power_destroyed}`
+            }});
+          } else {
+            message.channel.send({embed: {
+              color: 3447003,
+              description: `${sender}, please use \`!pd abc\`, where abc is a number or an actual person!}`
+            }});
+          }
         }
       } else {
         const top10 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY power_destroyed DESC LIMIT 10;").all(message.guild.id);
