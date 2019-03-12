f029508377303fd80bdcdebd00ea07b14ef532f6
Michael Thomas <noscere1978@gmail.com>
2019-03-12 17:36:08 +0000

    Add the null test as a seperate if statement

--- format.js
+++ format.js
@@ -1,5 +1,9 @@
 module.exports = {
   numberWithCommas: function (x) {
+    if(x == null) {
+      return x;
+    }
+
     if(!isNaN(x)) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
