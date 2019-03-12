b9b1b1bfd452b9ab47f9f07372fd898ab0d06666
Michael Thomas <noscere1978@gmail.com>
2019-03-12 17:30:09 +0000

    Unhandle exception when x is not a number (or null) .. so now we only process if it is a number, and return whatever was passed in if not.

--- format.js
+++ format.js
@@ -1,5 +1,10 @@
 module.exports = {
   numberWithCommas: function (x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    if(!isNaN(x)) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    else {
+      return x;
+    }
   }
 }
