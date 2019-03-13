0e94c66e3ba5be209f0491f22ff6eefa79eed1fa
Michael Thomas <noscere1978@gmail.com>
2019-03-13 18:58:43 +0000

    Commands !rr and !pd now refactored into switch statement for number of arguements.
    Members of the Admin group can add other members into the database.
    
    TODO: Refactor these for duplicate code, and for seperating into a 
    library

--- bot.js
+++ bot.js
@@ -133,53 +133,102 @@ bot.on('message', async message => {
             }
             logger.info(`Created score: ${score}`);
           }
-          if(args.length > 0) {
-            logger.info(`Args detected`);
-            if(!isNaN(args[0])) {
-              // Second argument is a number, update the score to this value
-              score.power_destroyed = args[0];
-              if(score.id == null) {
-                db.scores.add(score)
-                  .then(function(result) {
-                    // notify the user it was successful
-                    message.channel.send({embed: {
-                      color: 3447003,
-                      description: `Thank you, ${sender}, your power destroyed is set to ${args[0]}`
-                    }});
-                  })
-              } else {
-                db.scores.update(score)
-                  .then(function(result) {
-                    // notify the user it was successful
-                    message.channel.send({embed: {
-                      color: 3447003,
-                      description: `Thank you, ${sender}, your power destroyed is set to ${args[0]}`
-                    }});
-                  })
+          switch (args.length) {
+            case 2:
+              if(args.length > 1) {
+                let member = message.mentions.members.first();
+                if(member && !isNaN(args[1])) {
+                  // We have a !pd @name number
+                  // Admin only command
+                  let allowedRole = message.guild.roles.find("name", "Admin");
+                  if (message.member.roles.has(allowedRole.id)) {
+                    // allowed access to command
+                    db.scores.findByNameAndGuild(member.id, message.guild.id)
+                      .then (score => {
+                        if (score == null) {
+                          score = {
+                            uid: member.id,
+                            guild: message.guild.id,
+                            power_destroyed: 0,
+                            resources_raided: 0
+                          }
+                          logger.info(`Created score: ${score}`);
+                        }
+                        score.power_destroyed = args[1];
+                        if(score.id == null) {
+                          db.scores.add(score)
+                            .then(function(result) {
+                              // notify the user it was successful
+                              message.channel.send({embed: {
+                                color: 3447003,
+                                description: `Thank you, ${sender}, ${member.displayName} power destroyed is set to ${formatter.numberWithCommas(score.power_destroyed)}`
+                            }});
+                          })
+                        } else {
+                          db.scores.update(score)
+                            .then(function(result) {
+                              // notify the user it was successful
+                              message.channel.send({embed: {
+                                color: 3447003,
+                                description: `Thank you, ${sender}, ${member.displayName} power destroyed is set to ${formatter.numberWithCommas(score.power_destroyed)}`
+                              }});
+                          })
+                        }
+                    });
+                  }
                 }
-            } else {
-              let member = message.mentions.members.first();
-              if(member) {
-                db.scores.findByNameAndGuild(member.id, message.guild.id)
-                  .then (score => {
-                    let desc = `Unable to find ${member.displayName} in my database.  They need to log their scores for you to view them!`;
-                    if(score!=null) {
-                      desc = `${member.displayName} power destroyed is ${formatter.numberWithCommas(score.power_destroyed)}`
-                    }
-                    message.channel.send({embed: {
-                      color: 3447003,
-                      description: `${desc}`
-                    }
-                  });
-                })
-              } else {
-                message.channel.send({embed: {
-                  color: 3447003,
-                  description: `${sender}, please use \`!pd abc\`, where abc is a number or an actual person!}`
-                }});
               }
-            }
-          } else {
+            break;
+
+            case 1:
+              logger.info(`Args detected`);
+              if(!isNaN(args[0])) {
+                // Second argument is a number, update the score to this value
+                score.power_destroyed = args[0];
+                if(score.id == null) {
+                  db.scores.add(score)
+                    .then(function(result) {
+                      // notify the user it was successful
+                      message.channel.send({embed: {
+                        color: 3447003,
+                        description: `Thank you, ${sender}, your power destroyed is set to ${formatter.numberWithCommas(score.power_destroyed)}`
+                      }});
+                    })
+                  } else {
+                    db.scores.update(score)
+                      .then(function(result) {
+                        // notify the user it was successful
+                        message.channel.send({embed: {
+                          color: 3447003,
+                          description: `Thank you, ${sender}, your power destroyed is set to ${formatter.numberWithCommas(score.power_destroyed)}`
+                        }});
+                    })
+                  }
+              } else {
+                let member = message.mentions.members.first();
+                if(member) {
+                  db.scores.findByNameAndGuild(member.id, message.guild.id)
+                    .then (score => {
+                      let desc = `Unable to find ${member.displayName} in my database.  They need to log their scores for you to view them!`;
+                      if(score!=null) {
+                        desc = `${member.displayName} power destroyed is ${formatter.numberWithCommas(score.power_destroyed)}`
+                      }
+                      message.channel.send({embed: {
+                        color: 3447003,
+                        description: `${desc}`
+                      }
+                    });
+                  })
+                } else {
+                  message.channel.send({embed: {
+                    color: 3447003,
+                    description: `${sender}, please use \`!pd abc\`, where abc is a number or an actual person!}`
+                  }});
+                }
+              }
+            break;
+
+          case 0:
             db.manyOrNone("SELECT * FROM scores WHERE guild = $1 ORDER BY power_destroyed DESC LIMIT 10;", message.guild.id)
               .then(top10 => {
                 const embed = new Discord.RichEmbed()
@@ -201,19 +250,66 @@ bot.on('message', async message => {
 
     case 'resourcesraided':
     case 'rr':
-      var score = [];
-      db.scores.findByNameAndGuild(message.author.id, message.guild.id)
-        .then (score => {
-          if (score == null) {
-            score = {
-              uid: message.author.id,
-              guild: message.guild.id,
-              power_destroyed: 0,
-              resources_raided: 0
-            }
-            logger.info(`Created score: ${score}`);
+    var score = [];
+    db.scores.findByNameAndGuild(message.author.id, message.guild.id)
+      .then (score => {
+        if (score == null) {
+          score = {
+            uid: message.author.id,
+            guild: message.guild.id,
+            power_destroyed: 0,
+            resources_raided: 0
           }
-          if(args.length > 0) {
+          logger.info(`Created score: ${score}`);
+        }
+        switch (args.length) {
+          case 2:
+            if(args.length > 1) {
+              let member = message.mentions.members.first();
+              if(member && !isNaN(args[1])) {
+                // We have a !pd @name number
+                // Admin only command
+                let allowedRole = message.guild.roles.find("name", "Admin");
+                if (message.member.roles.has(allowedRole.id)) {
+                  // allowed access to command
+                  db.scores.findByNameAndGuild(member.id, message.guild.id)
+                    .then (score => {
+                      if (score == null) {
+                        score = {
+                          uid: member.id,
+                          guild: message.guild.id,
+                          power_destroyed: 0,
+                          resources_raided: 0
+                        }
+                        logger.info(`Created score: ${score}`);
+                      }
+                      score.resources_raided = args[1];
+                      if(score.id == null) {
+                        db.scores.add(score)
+                          .then(function(result) {
+                            // notify the user it was successful
+                            message.channel.send({embed: {
+                              color: 3447003,
+                              description: `Thank you, ${sender}, ${member.displayName} resources raided is set to ${formatter.numberWithCommas(score.resources_raided)}`
+                          }});
+                        })
+                      } else {
+                        db.scores.update(score)
+                          .then(function(result) {
+                            // notify the user it was successful
+                            message.channel.send({embed: {
+                              color: 3447003,
+                              description: `Thank you, ${sender}, ${member.displayName} resources raided is set to ${formatter.numberWithCommas(score.resources_raided)}`
+                            }});
+                        })
+                      }
+                  });
+                }
+              }
+            }
+          break;
+
+          case 1:
             logger.info(`Args detected`);
             if(!isNaN(args[0])) {
               // Second argument is a number, update the score to this value
@@ -224,17 +320,17 @@ bot.on('message', async message => {
                     // notify the user it was successful
                     message.channel.send({embed: {
                       color: 3447003,
-                      description: `Thank you, ${sender}, your resources raided is set to ${args[0]}`
+                      description: `Thank you, ${sender}, your resources raided is set to ${score.resources_raided}`
                     }});
                   })
-              } else {
-                db.scores.update(score)
-                  .then(function(result) {
-                    // notify the user it was successful
-                    message.channel.send({embed: {
-                      color: 3447003,
-                      description: `Thank you, ${sender}, your resources raided is set to ${args[0]}`
-                    }});
+                } else {
+                  db.scores.update(score)
+                    .then(function(result) {
+                      // notify the user it was successful
+                      message.channel.send({embed: {
+                        color: 3447003,
+                        description: `Thank you, ${sender}, your resources raided is set to ${score.resources_raided}`
+                      }});
                   })
                 }
             } else {
@@ -259,26 +355,27 @@ bot.on('message', async message => {
                 }});
               }
             }
-          } else {
-            db.manyOrNone("SELECT * FROM scores WHERE guild = $1 ORDER BY resources_raided DESC LIMIT 10;", message.guild.id)
-              .then(top10 => {
-                const embed = new Discord.RichEmbed()
-                  .setTitle("Resources Raided Leaderboard")
-                  .setAuthor(bot.user.username, bot.user.avatarURL)
-                  .setDescription("Our top 10 resources raided leaders!")
-                  .setColor(0x770086);
-                var c = 1;
-                for(const data of top10) {
-                  embed.addField(`${c}. ${bot.guilds.get(guildID).members.get(data.uid).displayName}`, `${formatter.numberWithCommas(data.resources_raided)}`);
-                  //embed.addField(`${c}. ${bot.users.get(data.uid).tag}`, `${formatter.numberWithCommas(data.resources_raided)}`);
-                  c++;
-                }
-                embed.addField(`Your personal resources raided is`, `${formatter.numberWithCommas(score.resources_raided)}`)
-                return message.channel.send({embed});
-              });
-          }
-        });
-    break;
+          break;
+
+        case 0:
+          db.manyOrNone("SELECT * FROM scores WHERE guild = $1 ORDER BY resources_raided DESC LIMIT 10;", message.guild.id)
+            .then(top10 => {
+              const embed = new Discord.RichEmbed()
+                .setTitle("Resources Raided Leaderboard")
+                .setAuthor(bot.user.username, bot.user.avatarURL)
+                .setDescription("Our top 10 resources raided leaders!")
+                .setColor(0x770086);
+              var c = 1;
+              for(const data of top10) {
+                embed.addField(`${c}. ${bot.guilds.get(guildID).members.get(data.uid).displayName}`, `${formatter.numberWithCommas(data.resources_raided)}`);
+                c++;
+              }
+              embed.addField(`Your personal resources raided is`, `${formatter.numberWithCommas(score.resources_raided)}`)
+              return message.channel.send({embed});
+            });
+        }
+      });
+  break;
   }
 });
 
