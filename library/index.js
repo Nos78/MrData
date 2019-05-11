170384b51c788dcd7bb30e591fdc149f50cae499
BanderDragon <noscere1978@gmail.com>
2019-05-11 01:34:09 +0100

    Added a collateArgs function to the library (effectively just a join function, starting at a given index of a given args array).

--- library/index.js
+++ library/index.js
@@ -1,3 +1,13 @@
+/**
+ * @Author: BanderDragon
+ * @Date:   2019-05-06T08:09:56+01:00
+ * @Email:  noscere1978@gmail.com
+ * @Project: MrData
+ * @Filename: index.js
+ * @Last modified by:   BanderDragon
+ * @Last modified time: 2019-05-11T01:23:08+01:00
+ */
+
 'use strict';
 
 // Renaming and exporting all library classes:
@@ -6,5 +16,16 @@ const admin = require('./admin/admin.js');
 
 module.exports = {
   Format: format,
-  Admin: admin
+  Admin: admin,
+
+  collateArgs: function(index, args) {
+      var returnString = "";
+      if (index < args.length) {
+          var returnString = args[index];
+          for(var i = index + 1; i < args.length; i++) {
+              returnString = " " + args[i];
+          }
+      }
+      return returnString;
+  }
 }
