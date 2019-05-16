eaf03b76f095d6ffa5a3437f4c08045cf126640e
BanderDragon <noscere1978@gmail.com>
2019-05-16 23:21:38 +0100

    Send message command allows admins to send a message as though they are the bot.

---
+++ commands/sendmessage.js
@@ -0,0 +1,31 @@
+/**
+ * @Author: BanderDragon
+ * @Date:   2019-05-16T22:04:42+01:00
+ * @Email:  noscere1978@gmail.com
+ * @Project: MrData
+ * @Filename: sendmessage.js
+ * @Last modified by:   BanderDragon
+ * @Last modified time: 2019-05-16T23:02:45+01:00
+ */
+
+const library = require('../library');
+
+module.exports = {
+    name: 'sendmessage',
+    description: `Use this command to send a message as the bot.  Your message will be deleted.  Only administrators can use this command.`,
+    aliases: ['send', 'sendmsg'],
+    usage: '<message>`',
+    args: true,
+    guildOnly: true,
+    execute(message, args) {
+        var userToChange = null;
+        var newNickname = "";
+
+        if (library.Admin.isAdmin(message.author.id, message.guild.id, message.client)) {
+        {
+            msgToSend = library.collateArgs(0, args);
+            message.channel.send(msgToSend)
+            message.delete();
+        }
+    },
+}
