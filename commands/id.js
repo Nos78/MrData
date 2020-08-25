03d48b3631e680cf4e2ae9ff5a20d4c070a9f0f2
noscere <michael@thebotfactory.net>
2020-08-25 03:13:04 +0100

    Added the displayName library wrapper, which wraps the discord API instead of calling it directly.

--- commands/id.js
+++ commands/id.js
@@ -1,43 +1,46 @@
-/**
+/*
  * @Author: BanderDragon
- * @Date:   2019-05-05T18:00:48+01:00
- * @Email:  noscere1978@gmail.com
- * @Project: MrData
- * @Filename: id.js
- * @Last modified by:   BanderDragon
- * @Last modified time: 2019-05-05T18:45:40+01:00
+ * @Date: 2020-08-25 02:54:25 
+ * @Last Modified by:   BanderDragon
+ * @Last Modified time: 2020-08-25 02:54:25 
  */
 
- const config = require('../config.json');
+const config = require('../config.json');
+const library = require('../library');
 
- module.exports = {
- 	name: 'id',
- 	description: 'Returns the discord Id of the specified user.  If no parameter is specified, it returns the sender\'s Id',
- 	cooldown: 60,
- 	args: false,
+module.exports = {
+  name: 'id',
+  description: 'Returns the discord Id of the specified user.  If no parameter is specified, it returns the sender\'s Id',
+  cooldown: 60,
+  args: false,
   usage: '<@memberName>',
- 	async execute(message, args) {
+  async execute(message, args) {
     var findThisUser = null;
     var desc = "";
+    let member = null;
+    let displayName = null;
 
     switch (args.length) {
       case 0:
         // No args specified, use the senders discord id.
         findThisUser = message.author.id;
         desc = `${message.author}, your id is `;
-        break;
+      break;
+
       case 1:
         member = message.mentions.members.first();
-        desc = `${message.author}, the id of ${member.displayname} is `;
+        displayName = library.Helper.displayName(member);
+
+        desc = `${message.author}, the id of ${displayName} is `;
         findThisUser = member.id;
-        break;
-     }
-     if(findThisUser) {
-       desc = desc + findThisUser;
-       message.channel.send({embed: {
-         color:config.standardMessageColor,
-         description: `${desc}`
-       }});
-     }
- 	},
- };
+      break;
+    }
+    if(findThisUser) {
+      desc = desc + findThisUser;
+      message.channel.send({embed: {
+        color:config.standardMessageColor,
+        description: `${desc}`
+      }});
+    }
+  },
+};
