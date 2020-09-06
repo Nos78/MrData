8b9bfd32978f621b223d89c7d713fbc375572acc
noscere <michael@thebotfactory.net>
2020-09-06 02:23:21 +0100

    New function to get userGuildSettings object

--- library/system/system.js
+++ library/system/system.js
@@ -43,5 +43,17 @@ module.exports = {
                 });
         }
         return settings;
+    },
+
+    getUserGuildSettings: function(userId, guildId) {
+        var settings = null;
+        if(userId && userId != 'null' && guildId && guildId != 'null') {
+            db.userGuildSettings.findGuildSettingsById(userId, guildId)
+                .then(result => {
+                    return Settings.getUserGuildSettingsFromRecord(result);
+                });
+        }
+        return settings;
     }
+
 }
