884d64b4bc94e3340fa0f1583a794d688f716a1a
noscere <michael@thebotfactory.net>
2020-09-28 21:01:38 +0100

    Fixed the definition of resolveCommandDescriptions & resolveCommandDescription to include the function keyword, as per the other function definitions.
    commandHelp:
    Refactoring datahelp - this new function to output a specific command help into an array

--- library/commands/commands.js
+++ library/commands/commands.js
@@ -2,7 +2,7 @@
  * @Author: BanderDragon 
  * @Date: 2020-09-08 20:02:01 
  * @Last Modified by: BanderDragon
- * @Last Modified time: 2020-09-10 16:46:13
+ * @Last Modified time: 2020-09-28 20:58:44
  */
 
 // Required modules
@@ -106,17 +106,65 @@ module.exports = {
         return returnValue;
     },
 
-    resolveCommandDescriptions(client) {
+    resolveCommandDescriptions: function(client) {
         var templates = global.library.Config.getHelpTextParameters(client);
         client.commands.forEach(function(command) {
             command.description = this.resolveCommandDescription(command, templates);
         }.bind(this));
     },
 
-    resolveCommandDescription(command, templates) {
+    resolveCommandDescription: function(command, templates) {
         templates.forEach(function(template) {
             command.description = command.description.replace(template.name, template.value);
         });
         return command.description;
+    },
+
+    /**
+     * Get the named command from the commands array stored in the client
+     * @param {string} commandName the name of the command being looked up
+     * @param {Object} client 
+     */
+    getCommand: function(commandName, client) {
+        // Get the specified command from the client
+        const cmd = client.commands.get(commandName) ||
+            client.commands.find(command => command.aliases && command.aliases.includes(commandName));
+        return cmd;
+    },
+
+    /**
+     * outputs the various parts of the command help text into an array. The array is passed into the function,
+     * and the parts of the help text are pushing into this array. The function returns true if the array was
+     * modified.
+     * @param {string} command 
+     * @param {string} prefix 
+     * @param {Array} data
+     * @returns {boolean} whether the array was modified.
+     */
+    commandHelp: function(command, prefix, data, client) {
+        var returnValue = false;
+        if(command) {
+            data.push(`**Name:** ${command.name}`);
+
+            if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+            if (command.description) data.push(`**Description:** ${command.description}`);
+            if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+            if(command.channel) {
+                data.push(`**Channel:** ${command.channel} - if this channel exists on your server, ${library.Config.botName(client)} will output the ranking table here.`);
+            }
+            if(command.category) {
+                data.push(`**Category:** ${command.category}`);
+            }
+            if(command.version) {
+                data.push(`**Version:** v${command.version}`);
+            }
+
+            data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+            returnValue = true;
+        } else {
+            logger.error(`commandHelp: command was not specified.`)
+        }
+        return returnValue;
     }
 }
