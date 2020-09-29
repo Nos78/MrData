f66920d59390ffab141ec66f97c9b82cdfe7a151
noscere <michael@thebotfactory.net>
2020-09-29 03:22:49 +0100

    If the creator is a member of the guild, pass their member object to the userCard function

--- commands/creator.js
+++ commands/creator.js
@@ -2,25 +2,31 @@
  * @Author: BanderDragon 
  * @Date: 2020-09-28 23:00:57 
  * @Last Modified by: BanderDragon
- * @Last Modified time: 2020-09-29 02:50:46
+ * @Last Modified time: 2020-09-29 03:21:45
  */
 
 const config = require('../config.json');
 const library = require('../library');
 const logger = require('winston');
+const { guildOnly } = require('./isowner');
 
 module.exports = {
     name: 'creator',
     description: 'Returns the discord username of my creator.',
     cooldown: 60,
     args: false,
+    version: '0.1.0',
     category: 'utility',
     async execute(message, args) {
         const creatorID = global.library.Admin.botOwnerId();
         msg = library.Helper.sendStandardWaitMessage(message.channel);
         message.client.fetchUser(creatorID)
             .then(function (creatorUser) {
-                var embedMsg = global.library.Helper.userCard(creatorUser, message.channel, message.client);
+                var member = null;
+                if(message.guild.member(creatorID)) {
+                    member = message.guild.members.get(creatorID);
+                }
+                var embedMsg = global.library.Helper.userCard(creatorUser, message.channel, message.client, member);
                 library.Helper.editMessageEmbed(msg, embedMsg);
         }.bind(this));
     }
