99e3de71e68dbf189e44ac0b0e7fd8224efb9f10
noscere <michael@thebotfactory.net>
2020-09-30 17:54:42 +0100

    Renamed the 'guild' command to be 'guildid' - added an alias of 'gid'.
    This allows the 'guild' command to be a new alias for 'guildinformation'.

--- commands/guildid.js
+++ commands/guildid.js
@@ -2,17 +2,17 @@
  * @Author: BanderDragon 
  * @Date: 2019-05-05 18:46:55
  * @Last Modified by: BanderDragon
- * @Last Modified time: 2020-09-29 02:57:13
+ * @Last Modified time: 2020-09-30 17:47:54
  */
 
 const config = require('../config.json');
 
 module.exports = {
   name: 'guildid',
-  aliases: ['guild'],
+  aliases: ['serverid', 'gid'],
   description: 'Returns the discord Id of the current guild (server).',
   cooldown: 60,
-  version: '1.0.0',
+  version: '1.0.1',
   category: 'utility',
   args: false,
   async execute(message, args) {
